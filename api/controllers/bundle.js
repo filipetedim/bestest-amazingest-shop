ad4ff13cf5c55b2c3729e618d4890881bf0792f2
Filipe Tedim <filipetedim@gmail.com>
2019-03-28 06:19:36 +0000

    Changed bundle API to return products too

--- api/controllers/bundle.js
+++ api/controllers/bundle.js
@@ -1,14 +1,48 @@
 // Package dependencies
+const Mongoose = require('mongoose');
 const { validationResult } = require('express-validator/check');
 
 // Models
 const Bundle = require('../models/Bundle');
+
+const Product = Mongoose.model('Product');
+
+/**
+ * Adds the product information to each bundle.
+ */
+const addProducts = async result => {
+  if (Array.isArray(result)) {
+    await Promise.all(
+      result.map((bundle, i) =>
+        Promise.all(
+          bundle.products.map((productId, j) =>
+            Product.findById(productId).then(product => {
+              result[i].products[j] = product; // eslint-disable-line
+            }),
+          ),
+        ),
+      ),
+    );
+
+    return result;
+  }
+
+  await Promise.all(
+    result.products.map((productId, i) =>
+      Product.findById(productId).then(product => {
+        result.products[i] = product; // eslint-disable-line
+      }),
+    ),
+  );
+  return result;
+};
 
 /**
  * Gets all bundles.
  */
 const getBundles = (req, res) => {
   Bundle.find()
+    .then(addProducts)
     .then(result => res.status(200).json(result))
     .catch(error => res.status(500).json({ message: 'Something went wrong', error }));
 };
@@ -20,6 +54,7 @@ const getBundle = (req, res) => {
   const { id } = req.params;
 
   Bundle.findById(id)
+    .then(addProducts)
     .then(result => res.status(result ? 200 : 404).json(result || {}))
     .catch(error => res.status(500).json({ message: 'Something went wrong', error }));
 };
@@ -53,6 +88,7 @@ const updateBundle = (req, res) => {
   const { name, discountPercentage, products } = req.body;
 
   Bundle.findByIdAndUpdate(id, { name, discountPercentage, products }, { new: true })
+    .then(addProducts)
     .then(result => res.status(result ? 200 : 404).json(result || {}))
     .catch(error => res.status(500).json({ message: 'Something went wrong', error }));
 };
@@ -75,6 +111,7 @@ const getBundlesWithProductId = (req, res) => {
   const { id } = req.params;
 
   Bundle.find({ products: id })
+    .then(addProducts)
     .then(result => res.status(200).json(result))
     .catch(error => res.status(500).json({ message: 'Something went wrong', error }));
 };
